41b74d01561506e96ae337bf98248721e34b0f89
Maurice Fiorani <mauricef97@gmail.com>
2020-02-19 20:24:26 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.backed_project(self) unless backer.backed_projects.include?(self)
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
   
 end  
